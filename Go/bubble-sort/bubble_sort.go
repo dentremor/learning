34bef070d3c894786a479328f007d411139c8347
dentremor <hillerdaniel11@gmail.com>
2021-11-05 19:28:52 +0100

    feat: Add go

---
+++ Go/bubble-sort/bubble_sort.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := [8]int{7, 4, 6, 2, 5, 3, 8, 1}
+
+	for i := 0; i < len(numbers); i++ {
+		for j := 0; j < len(numbers)-1; j++ {
+			if numbers[j] > numbers[j+1] {
+				cache := numbers[j]
+				numbers[j] = numbers[j+1]
+				numbers[j+1] = cache
+			}
+		}
+	}
+
+	fmt.Println(numbers)
+}
---
+++ Go/todo-list/todo_list.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("GoDo - Your todo manager ")
+
+}
+
+func showAllTasks() {
+
+}
+
+func showPendingTasksToday() {
+
+}
+
+func deleteTask() {
+
+}
+
+func createTask() {
+
+}
